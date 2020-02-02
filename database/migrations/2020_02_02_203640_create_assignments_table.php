7ffddac28c16aabe9fa6aa356602a03108069c81
Michał Szkadłubowicz <valheru1@gmail.com>
2020-02-02 22:19:12 +0100

    first commit

---
+++ database/migrations/2020_02_02_203640_create_assignments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('who_set');
+            $table->string('who_get');
+            $table->integer('priority')->nullable();
+            $table->string('title');
+            $table->text('body')->nullable();
+            $table->timestamp('completed')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
